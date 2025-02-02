e7f30fa912231bc9af3c095740606d02fa69f256
edu-fr <edu.felipe1@outlook.com>
2025-02-02 15:58:55 -0300

    updating label from health bar

--- assets/scripts/UI/health_progress_bar.gd
+++ assets/scripts/UI/health_progress_bar.gd
@@ -3,17 +3,23 @@ class_name HealthBarController
 extends Control
 
 @export var progressBar : ProgressBar
+@export var label : Label
 
-func init(max : int, current : int):
-	progressBar.max_value = max
+func init(_max : int, _current : int) -> void:
+	progressBar.max_value = _max
 	progressBar.min_value = 0
-	progressBar.value = current
-	progressBar.changed
+	progressBar.value = _current
+	update_label()
 
-	return self
+	return
 
-func take_damage(damageValue : int):
-	progressBar.value -= damageValue
+func take_damage(_damageValue : int) -> void:
+	progressBar.value -= _damageValue
+	update_label()
 
-func heal(healValue : int):
-	progressBar.value += healValue
+func heal(_healValue : int) -> void:
+	progressBar.value += _healValue
+	update_label()
+
+func update_label() -> void:
+	label.text = "{0}/{1}".format([progressBar.value, progressBar.max_value])
